34555a0507e833e1fe2f4d4db8b63861548823bf
Gustavo Paco <gustavopaco@gmail.com>
2021-08-19 11:34:52 -0300

    1- Modulo 1 de fundamentos Javascript completo, com exemplos na pasta Pratica e Examples

--- app/js/index.js
+++ app/js/index.js
@@ -2,7 +2,7 @@
 require('styles/main.scss');
 /* js */
 import $ from 'jquery';
-import { log, logTitle } from 'logger';
+import {log, logTitle} from 'logger';
 /* your imports */
 logTitle('Title')
 /* coding examples */
